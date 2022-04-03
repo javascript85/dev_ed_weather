26c231657c552872847cad2d297a58eca11dedb9
javascript85 <99102746+javascript85@users.noreply.github.com>
2022-04-03 08:39:50 +0200

    js

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-const http = require("http");
+window.addEventListener('load', ()=>{
+    let long;
+    let lat;
+
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(position=>{
+            console.log(position);
+        })
+    }else{
+        h1.textComtent = "hey this is not workinf because some reason"
+    }
+});
